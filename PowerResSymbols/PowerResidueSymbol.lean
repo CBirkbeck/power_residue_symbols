10e00a664ffbfd1f85f467d57db5cf2a6e55726f
Sandra Rozensztajn <sandra.rozensztajn@ens-lyon.fr>
2024-03-25 17:42:15 +0100

    first try

--- PowerResSymbols/PowerResidueSymbol.lean
+++ PowerResSymbols/PowerResidueSymbol.lean
@@ -4,28 +4,44 @@ import Mathlib.RingTheory.Ideal.Norm
 open scoped NumberField BigOperators
 
 variable {F : Type*} [Field F] [NumberField F] (ζ : 𝓞 F) (n : ℕ) (h : IsPrimitiveRoot ζ n)
+variable (p : Ideal (𝓞 F)) (hp : Ideal.IsPrime p) (hp2: p ≠ ⊥)
 
 /--The residue field of a number field (specifically the ring of intergers) at a prime-/
 def ResidueFieldAtPrime (p : Ideal (𝓞 F)) (hp : Ideal.IsPrime p) :=
   LocalRing.ResidueField (Localization.AtPrime p)
 
 /--The residue field of a number field (specifically the ring of intergers) at a prime-/
-def ResidueFieldAtPrime2 (p : Ideal (𝓞 F)) (hp : Ideal.IsPrime p) :=  𝓞 F ⧸ p
+def ResidueFieldAtPrime2 (p : Ideal (𝓞 F)) (hp : Ideal.IsPrime p)
+  :=  𝓞 F ⧸ p
 
-variable (p : Ideal (𝓞 F)) (hp : Ideal.IsPrime p) (hp2: p ≠ ⊥)
 
 noncomputable section
+
 
 noncomputable instance : Field (ResidueFieldAtPrime p hp) := by
   apply LocalRing.ResidueField.field
 
-noncomputable instance (hp2 : p ≠ ⊥) : Field (ResidueFieldAtPrime2 p hp) := by
+def residue_map : 𝓞 F →+* (ResidueFieldAtPrime p hp) :=
+  (LocalRing.residue (Localization.AtPrime p)).comp (algebraMap (𝓞 F) (Localization.AtPrime p))
+
+noncomputable instance : CommRing (ResidueFieldAtPrime2 p hp) := by
+  apply Ideal.Quotient.commRing
+
+def residue_map2 : 𝓞 F →+* (ResidueFieldAtPrime2 p hp)  := by
+  have := Ideal.Quotient.mk p
+  unfold ResidueFieldAtPrime2
+  convert this
+
+/-
+noncomputable instance : Field (ResidueFieldAtPrime2 p hp) := by
   have h : Ideal.IsMaximal p := by
     apply Ideal.IsPrime.isMaximal hp hp2
   apply Ideal.Quotient.field
+-/
 
-def residue_map : 𝓞 F →+* (ResidueFieldAtPrime p hp) :=
-  (LocalRing.residue (Localization.AtPrime p)).comp (algebraMap (𝓞 F) (Localization.AtPrime p))
+
+
+
 
 instance   : Fintype (ResidueFieldAtPrime2 p hp) := by
   letI := Ideal.fintypeQuotientOfFreeOfNeBot p hp2
