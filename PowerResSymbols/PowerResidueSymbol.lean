a82bc28d62ba5e5793886eeb33576a2249b8a0d2
Edgar Costa <edgarc@mit.edu>
2024-03-25 17:46:37 +0100

    remove .lake

--- PowerResSymbols/PowerResidueSymbol.lean
+++ PowerResSymbols/PowerResidueSymbol.lean
@@ -5,21 +5,25 @@ open scoped NumberField BigOperators
 
 variable {F : Type*} [Field F] [NumberField F] (ζ : 𝓞 F) (n : ℕ) (h : IsPrimitiveRoot ζ n)
 
+variable (p : Ideal (𝓞 F)) (hp : Ideal.IsPrime p) (hp2: p ≠ ⊥)
+
 /--The residue field of a number field (specifically the ring of intergers) at a prime-/
-def ResidueFieldAtPrime (p : Ideal (𝓞 F)) (hp : Ideal.IsPrime p) :=
+def ResidueFieldAtPrime (hp : Ideal.IsPrime p) :=
   LocalRing.ResidueField (Localization.AtPrime p)
 
 /--The residue field of a number field (specifically the ring of intergers) at a prime-/
-def ResidueFieldAtPrime2 (p : Ideal (𝓞 F)) (hp : Ideal.IsPrime p) :=  𝓞 F ⧸ p
-
-variable (p : Ideal (𝓞 F)) (hp : Ideal.IsPrime p) (hp2: p ≠ ⊥)
+def ResidueFieldAtPrime2 :=  𝓞 F ⧸ p
 
 noncomputable section
 
 noncomputable instance : Field (ResidueFieldAtPrime p hp) := by
   apply LocalRing.ResidueField.field
 
-noncomputable instance (hp2 : p ≠ ⊥) : Field (ResidueFieldAtPrime2 p hp) := by
+noncomputable instance : CommRing (ResidueFieldAtPrime2 p) := by
+  apply Ideal.Quotient.commRing
+
+
+noncomputable instance (hp2 : p ≠ ⊥) : Field (ResidueFieldAtPrime2 p) := by
   have h : Ideal.IsMaximal p := by
     apply Ideal.IsPrime.isMaximal hp hp2
   apply Ideal.Quotient.field
@@ -27,12 +31,21 @@ noncomputable instance (hp2 : p ≠ ⊥) : Field (ResidueFieldAtPrime2 p hp) := 
 def residue_map : 𝓞 F →+* (ResidueFieldAtPrime p hp) :=
   (LocalRing.residue (Localization.AtPrime p)).comp (algebraMap (𝓞 F) (Localization.AtPrime p))
 
-instance   : Fintype (ResidueFieldAtPrime2 p hp) := by
+
+
+def residue_map2 : 𝓞 F →+* (ResidueFieldAtPrime2 p) := by
+  have := Ideal.Quotient.mk p
+  unfold ResidueFieldAtPrime2
+  convert this
+
+
+instance   : Fintype (ResidueFieldAtPrime2 p) := by
   letI := Ideal.fintypeQuotientOfFreeOfNeBot p hp2
   convert this
 
-lemma l1 [Fintype (ResidueFieldAtPrime2 p hp)] :
-  Fintype.card (ResidueFieldAtPrime2 p hp) = Ideal.absNorm p := by sorry
+lemma l1 [Fintype (ResidueFieldAtPrime2 p)] :
+  Fintype.card (ResidueFieldAtPrime2 p) = Ideal.absNorm p := by
+  sorry
 
 lemma norm_div_lemmas (p : Ideal (𝓞 F)) (hp : Ideal.IsPrime p)
       (hpn : p ⊔ Ideal.span ({(n : (𝓞 F))} : Set (𝓞 F)) = ⊤) : n  ∣ ((Ideal.absNorm p) - 1) := by
